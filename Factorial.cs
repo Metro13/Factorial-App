9d81612e273902f83369d1b9b5459ce38e644fde
Metro13 <phirinoel289@gmail.com>
2022-03-06 11:09:44 +0200

    -1st-Commit

---
+++ Factorial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Factorial
+    {
+        private int number;
+        private int factor;
+
+        public int Number
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+
+        public int Factor
+        {
+            set
+            {
+                factor = value;
+            }
+            get
+            {
+                return factor;
+            }
+        }
+
+        public void CalculateFactorial()
+        {
+            Factor = 1;
+
+            for (int i = 1; i <= Number; i++)
+            {
+                Factor = Factor * i;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class Form1 : Form
+    {
+        Factorial factorial = new Factorial();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdCalculate_Click(object sender, EventArgs e)
+        {
+            factorial.Number = int.Parse(TxtNumber.Text);
+
+            factorial.CalculateFactorial();
+
+            MessageBox.Show("The Factorial of " + factorial.Number + " is " + factorial.Factor);
+                
+        }
+    }
+}
